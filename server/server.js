5cf0411ce5fb544620a8313395d05f2c8f888ec7
Evan Mencke <metaevan@gmail.com>
2015-08-31 10:50:02 -0700

    [feat] Add trulia api request function

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ var createDistanceMatrix = function (originsArr, destination) {
   service.getDistanceMatrix({
     origins: originsArr,
     destinations: [destination],
-    travelMode: google.maps.TravelMode."DRIVING",
+    travelMode: google.maps.TravelMode.DRIVING,
   }, function(response, status) {
     if (status !== google.maps.DistanceMatrixStatus.OK) {
       console.log('Error was: ' + status);
@@ -156,8 +156,41 @@ var reverseGeocode = function (searchInfo) {
   Website: Google places
 */
 
+//-----------------------------------------------------------------------------------
+//GET price of houses sold list, given a particular zip code
+//example request: http://api.trulia.com/webservices.php?library=TruliaStats&function=getZipCodeStats&zipCode=90404&startDate=2014-01-01&endDate=2015-08-01&statType=listings&apikey=<INSERT API KEY HERE>
+/*Prerequisites:
+	zip code
+  Website: Trulia
 
+  TODO:
+    change to real dates.
+    parse the XML
+*/
+var trulia = function (zip) {
+  var deferred = Q.defer();
 
+  var truliaUrl_address = 'http://api.trulia.com/webservices.php?library=TruliaStats&function=getZipCodeStats&zipCode='
+  var truliaUrl_start_date = '&startDate=2014-01-01'
+  var truliaUrl_end_date = '&endDate=2015-08-01&statType=listings&apikey=';
+
+  var truliaUrl = truliaUrl_address + zip + truliaUrl_start_date + truliaUrl_end_date + TruliaAPIKey;
+  console.log('Sample URL for Trulia:', truliaUrl);
+
+  http.get( truliaUrl, function (response) {
+      var body = '';
+      response.on('data', function (chunk) {
+        body += chunk;
+      });
+      response.on('end', function () {
+        //remove
+        //console.log('Trulia data - BODY: ' + body);
+        deferred.resolve(body);
+      });
+  }); //end of http.get
+
+  return deferred.promise;
+}
 
 
 
