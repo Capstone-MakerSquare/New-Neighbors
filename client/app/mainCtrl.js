3cd47c20530c15deef07b1492a02beb9d35d5765
Evan Mencke <metaevan@gmail.com>
2015-09-02 11:03:08 -0700

    adding files to dev branch to create a new branch

---
+++ client/app/mainCtrl.js
@@ -0,0 +1,44 @@
+angular.module('app',['app.services'])
+.controller('MainController', ['Api', function (Api){
+  var main = this;
+
+  main.address = '';
+  main.bedrooms = '';
+  main.bathrooms = '';
+  main.buyOrRent = 'rent';
+
+  main.searchInfo = { };
+
+  main.initMap = function() {
+
+  }
+
+  main.submitAddress = function() {
+    console.log('mainCtrl.js says: Submitted address:', main.address);
+
+    main.searchInfo.bedrooms = main.bedrooms;
+    main.searchInfo.bathrooms = main.bathrooms;
+    main.searchInfo.buyOrRent = main.buyOrRent;
+
+
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': main.address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results[0].geometry.location);
+        console.log('results address', results[0].formatted_address);
+        if (!isNaN(results[0].formatted_address[0])) {
+          main.searchInfo.address = results[0].formatted_address;
+          Api.submit(main.searchInfo);
+        } else {
+          alert("Please insert a valid U.S. address");
+        }
+      } else {
+        console.log('error: ', status, ' ', results)
+      }
+    });
+
+
+  };
+
+
+}]);
---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleAPIKey : 'AIzaSyCZR_sa7KotwYk8H1E3cGANBJKUr6cFsYg',
+  TruliaAPIKey : 'xafexeyxrm9ex2fmh2y84hbf'
+};
