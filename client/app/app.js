e9eaa1a08d786709812a6fe80378267d9df7ea28
Melinda Sarnicki <melindasarnicki@gmail.com>
2015-09-07 08:25:24 -0700

    [Feat] Implements ui-routing

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,43 @@
-angular.module('myApp', ['myApp.requestHoodServices', 'myApp.mapServices']);
+var app = angular.module('myApp', [
+  'myApp.requestHoodServices', 
+  'myApp.mapServices', 
+  'ui.router'
+])
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.
+    otherwise("/");
+
+  $stateProvider
+    .state('main', {
+      url: "/main",
+      controller: 'MainController',
+      controllerAs: 'main',
+      views: {
+        "": {
+          templateUrl: "./app/main/main.html"
+        }
+      }
+     })
+    .state('main.results', {
+      url: "/results",
+      controller: 'MainController',
+      controllerAs: 'main',
+      views: {
+        "glance-card": {
+          templateUrl: "./app/thumbnails/thumbnailsTemplate.html"
+        }
+      }
+    })
+    .state('main.details', {
+      url: "/details",
+      controller: 'MainController',
+      controllerAs: 'main',
+      views: {
+        "glance-card": {
+          templateUrl: "./app/details/detailsTemplate.html"
+        }
+      }
+    })
+  });
+
