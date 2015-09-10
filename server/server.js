9e69047933fcc1897c9419cd897dde1b0537c267
raghavabboy <raghavabboy@gmail.com>
2015-09-10 11:03:14 -0700

    [Save] Updates tags for amenities and attractions.

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ var queryAmenities = function (coordinates, neighborhood) {
 
   var amenitiesObj = {};
   var radius = 500;
-  var types = 'atm|bank|beauty_salon|book_store|cafe|car_rental|car_repair|car_wash|clothing_store|convenience_store|dentist|department_store|doctor|electrician|electronics_store|fire_station|florist|food|furniture_store|gas_station|general_contractor|grocery_or_supermarket|gym|hair_care|hardware_store|health|home_goods_store|hospital|insurance_agency|laundry|library|liquor_store|locksmith|meal_delivery|meal_takeaway|movie_rental|parking|pet_store|pharmacy|plumber|post_office|school|shoe_store|spa|store|subway_station|taxi_stand|train_station|veterinary_care|amusement_park|aquarium|art_gallery|bar|bowling_alley|casino|jewelry_store|movie_theatre|museum|night_club|park|restaurant|shopping_mall|stadium|university|zoo';
+  var types = 'airport|atm|bank|beauty_salon|book_store|cafe|car_rental|convenience_store|fire_station|food|gas_station|grocery_or_supermarket|gym|hospital|laundry|library|pharmacy|post_office|school|spa|store|subway_station|train_station|veterinary_care|amusement_park|aquarium|art_gallery|bar|bowling_alley|casino|movie_theatre|museum|night_club|park|restaurant|shopping_mall|stadium|university|zoo';
 
   var gAmenitiesUrl_location = 'https://maps.googleapis.com/maps/api/place/search/json?location=';
   var gAmenitiesUrl_radius = '&radius=';
