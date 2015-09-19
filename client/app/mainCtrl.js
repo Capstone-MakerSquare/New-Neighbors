0489d98a5ce677dac67519cac9d1b6f44450f9fa
Evan Mencke <metaevan@gmail.com>
2015-09-19 11:07:19 -0700

    [Feature] Adds deployment capability

--- client/app/mainCtrl.js
+++ client/app/mainCtrl.js
@@ -222,13 +222,13 @@ app.controller('MainController', ['Map', 'ServerApi', '$state', 'Details', 'Char
       main.neighborhoods = Object.keys(data).map(function(key) { return data[key]; });  //converts object of objects to array of objects
 
       //remove
-      // console.log('requestNeighborhoods says: main.neighborhoods:',main.neighborhoods);
+      console.log('requestNeighborhoods says: main.neighborhoods:',main.neighborhoods);
 
       main.attractionObj = Details.createPlacesObj(main.neighborhoods, Details.attractionDict);
       main.serviceObj = Details.createPlacesObj(main.neighborhoods, Details.serviceDict);
 
       //remove
-      console.log('requestNeighborhoods says: main.serviceObj:',main.serviceObj);
+      // console.log('requestNeighborhoods says: main.serviceObj:',main.serviceObj);
 
       main.getBuyPrice(main.neighborhoods);
       main.neighborhoodArray = main.orderByArray(main.neighborhoods);
@@ -273,7 +273,7 @@ app.controller('MainController', ['Map', 'ServerApi', '$state', 'Details', 'Char
 
 
     marker.addListener('click', function() {
-      console.log('neighborhood clicked:', neighborhoodObj);
+      // console.log('neighborhood clicked:', neighborhoodObj);
       if(neighborhoodObj.name === main.currentNeighborhood.name) { return; }
 
       main.selectNeighborhood(neighborhoodObj)
@@ -304,7 +304,7 @@ app.controller('MainController', ['Map', 'ServerApi', '$state', 'Details', 'Char
   //----------------------------------------------------------------------------------
   //Function to initialize and draw the map, centering on the the center of the U.S.
   main.initMap = function() {
-    console.log("main.initMap")
+    // console.log("main.initMap")
     var centerUS = {
       latitude: 38.5,
       longitude: -96
@@ -350,7 +350,7 @@ app.controller('MainController', ['Map', 'ServerApi', '$state', 'Details', 'Char
     }
 
     //remove
-    console.log("mapCurrentNeighborhood says: Details.currentNeighborhood:", Details.currentNeighborhood);
+    // console.log("mapCurrentNeighborhood says: Details.currentNeighborhood:", Details.currentNeighborhood);
   }
 
   //----------------------------------------------------------------------------------
