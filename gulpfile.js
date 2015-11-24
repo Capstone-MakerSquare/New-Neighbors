a600607384d05b5a5abc121d8595c2875d611b0a
KalisaFalzone <kalisa.falzone@gmail.com>
2015-11-24 15:33:29 -0800

    [Update] moves foundation icons reference to a cdn rather than downloading

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,22 @@
 
 var gulp = require('gulp');
 var del  = require('del');
-// css modules 
+// css modules
 var sourcemaps = require('gulp-sourcemaps');
 var replace = require('gulp-replace');
 var concatcss = require('gulp-concat-css');
 var uglifycss = require('gulp-uglifycss');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
-// js modules 
+// js modules
 var concat = require('gulp-concat');
-// image modules 
+// image modules
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-// server and live reload 
+// server and live reload
 var livereload = require('gulp-livereload');
 var nodemon = require('gulp-nodemon');
-// html 
+// html
 var htmlreplace = require('gulp-html-replace');
 
 // 2. FILE PATHS
@@ -39,7 +39,7 @@ var paths = {
     main: 'client/assets/css/styles.css'
   },
   styles: {
-    libsrcs: ['client/lib/foundation-icons/foundation-icons.css','client/lib/slick-carousel/slick/slick.css','client/lib/slick-carousel/slick/slick-theme.css'],
+    libsrcs: ['client/lib/slick-carousel/slick/slick.css','client/lib/slick-carousel/slick/slick-theme.css'],
     main: 'dist/assets/css',
     dist: 'dist/assets/*.css'
   },
@@ -68,9 +68,9 @@ var paths = {
     lib: 'client/lib/angular-slick/dist/slick.js',
     libdist: 'dist/lib',
     clientdist:['client/app/details/detailsController.js', 'client/app/thumbnails/thumbnailsController.js',
-    'client/app/map/mapDirective.js', 'client/app/searchForm/searchFormDirective.js', 
+    'client/app/map/mapDirective.js', 'client/app/searchForm/searchFormDirective.js',
     'client/app/filter/filterDirective.js', 'client/app/thumbnails/thumbnailsDirective.js',
-    'client/app/mainCtrl.js','client/app/charts/chartsController.js', 'client/app/charts/chartsDirective.js', 
+    'client/app/mainCtrl.js','client/app/charts/chartsController.js', 'client/app/charts/chartsDirective.js',
     'client/app/countup/countup.min.js', 'client/app/team/teamDirective.js']
   }
 };
@@ -83,27 +83,27 @@ gulp.task('clean', function() {
   return del(['dist']);
 });
 
-// reloads html file on change 
+// reloads html file on change
 gulp.task('html',['clean'], function() {
     return gulp.src(paths.html.main)
     .pipe(gulp.dest(paths.html.dist))
     .pipe(livereload());
 });
 
-// moves icon files to dist 
+// moves icon files to dist
 gulp.task('icons',['clean'], function() {
     return gulp.src(paths.icons.main)
     .pipe(gulp.dest(paths.styles.main));
 });
 
 
-// moves lib scripts to dist 
+// moves lib scripts to dist
 gulp.task('libjs',['clean'], function() {
     return gulp.src(paths.scripts.lib)
     .pipe(gulp.dest(paths.scripts.libdist));
 });
 
-// dynamically replaces script files in html file 
+// dynamically replaces script files in html file
 gulp.task('replacescripts', ['clean', 'html'], function() {
   return gulp.src('dist/index.html')
     .pipe(htmlreplace({
@@ -117,25 +117,25 @@ gulp.task('replacescripts', ['clean', 'html'], function() {
     .pipe(gulp.dest('dist'));
 });
 
-// moves font assets 
+// moves font assets
 gulp.task('font',['clean'], function() {
     return gulp.src(paths.font.main)
     .pipe(gulp.dest(paths.font.dist));
 });
 
-// move slick.js to dist 
+// move slick.js to dist
 gulp.task('slick',['clean'], function() {
     return gulp.src(paths.slick.main)
     .pipe(gulp.dest(paths.slick.dist));
 });
 
-// moves services.js files 
+// moves services.js files
 gulp.task('services',['clean'], function() {
     return gulp.src(paths.services.main)
     .pipe(gulp.dest(paths.services.dist));
 });
 
-//minifies and concats JS files 
+//minifies and concats JS files
 gulp.task('minjs', ['clean'], function() {
   return gulp.src(paths.scripts.clientdist)
     .pipe(concat('app.min.js'))
@@ -143,7 +143,7 @@ gulp.task('minjs', ['clean'], function() {
     .pipe(gulp.dest(paths.scripts.dist));
 });
 
-// reloads js file on change 
+// reloads js file on change
 gulp.task('js', ['clean'], function() {
   return gulp.src(paths.scripts.app)
     .pipe(gulp.dest(paths.scripts.appdist))
@@ -162,7 +162,7 @@ gulp.task('css', ['clean'], function() {
     .pipe(livereload());
 });
 
-// minifies lib css files 
+// minifies lib css files
 gulp.task('libcss',['clean'], function() {
   return gulp.src(paths.styles.libsrcs)
     .pipe(concatcss('lib.min.css'))
@@ -170,14 +170,14 @@ gulp.task('libcss',['clean'], function() {
     .pipe(gulp.dest(paths.styles.main));
 });
 
-// minifies images 
+// minifies images
 gulp.task('images', ['clean'], function () {
   return gulp.src(paths.images.main)
     .pipe(pngquant({quality: '65-80', speed: 4})())
     .pipe(gulp.dest(paths.images.dist));
 });
 
-// watches for changes 
+// watches for changes
 gulp.task('watch', function() {
   gulp.watch(paths.html.main, ['html']);
   gulp.watch(paths.scripts.app, ['minjs']);
@@ -192,13 +192,13 @@ gulp.task('serve', function() {
 // 4. RUN TASKS
 // - - - - - - - - - - - - - - -
 
-// task groupings 
+// task groupings
 gulp.task('htmlTasks', ['html', 'replacescripts']);
 gulp.task('cssTasks', ['css', 'libcss']);
 gulp.task('jsTasks', ['minjs', 'libjs', 'services']);
 gulp.task('assetsTasks', ['slick', 'font', 'icons', 'images']);
 
-// run tasks 
-gulp.task('default', ['watch','serve']); 
-gulp.task('dist', ['htmlTasks','cssTasks','jsTasks','assetsTasks']); 
+// run tasks
+gulp.task('default', ['watch','serve']);
+gulp.task('dist', ['htmlTasks','cssTasks','jsTasks','assetsTasks']);
 
